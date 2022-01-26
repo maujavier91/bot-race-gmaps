aa64d1a34aaa8e28c35f22dd4f0324d90ce8c232
Mauricio Naranjo <maujavier91@gmail.com>
2022-01-25 22:21:32 -0400

    formatting and comment functions

--- src/bot.js
+++ src/bot.js
@@ -1,104 +1,129 @@
-import { getRhumbLineBearing, computeDestinationPoint, getDistance } from 'geolib';
+import {
+  getRhumbLineBearing,
+  computeDestinationPoint,
+  getDistance
+} from "geolib";
 
 class Bot {
-    battery = 100
-    position
-    origin
-    destination
-    minEnergyStep = 10
-    maxEnergyStep = 30
-    minStep = 50
-    maxStep = 100
-    isAtDestination = false
-    color = '#FFFF00'
-    hasCharge = true
-    isCharging = false
-    label
+  battery = 100;
+  position;
+  origin;
+  destination;
+  minEnergyStep = 10;
+  maxEnergyStep = 30;
+  minStep = 50;
+  maxStep = 100;
+  isAtDestination = false;
+  color = "#FFFF00";
+  hasCharge = true;
+  isCharging = false;
+  label;
 
-
-    static earthRadius = 6378137 // in meters
-    constructor(label, destination) {
-        this.label = label
-        this.destination = destination
-        this.setRandomOrigin(this.destination)
-    }
-
-    setRandomOrigin(destination) {
-        this.position = {
-            lat: destination.lat + Bot.getRandomInt(0, 15)*0.01*(Math.random() < 0.5 ? -1 : 1),
-            lng: destination.lng + Bot.getRandomInt(0, 15)*0.01*(Math.random() < 0.5 ? -1 : 1)
-        }
-        this.origin = this.position
-    }
-
-    moveTowardsDestination(distance) {
-        if (this.getDistanceToDestination() !== 0) {
-            this.decreaseBattery(Bot.getRandomInt(10,30))
-        if (this.battery>=0 && this.hasCharge) {
-
-            if (this.getDistanceToDestination() > distance) {
-                let displacementPoint =  computeDestinationPoint(this.position, distance, getRhumbLineBearing(this.position, this.destination), Bot.earthRadius)
-                this.position = { lat: displacementPoint.latitude, lng: displacementPoint.longitude }
-            } else {
-                this.position = this.destination
-                this.isAtDestination = true;
-            }
-            
-        } else {
-            if (!this.isCharging) {
-                this.isCharging = true
-            setTimeout(() => {
-             this.rechargeBattery() 
-            }, 6000)
-            }
-            
-        }
-        }
-
-     }
-    
-    getDistanceToDestination() {
-/*         const rad = function(x) {
-            return x * Math.PI / 180;
+  static earthRadius = 6378137; // in meters
+  /**Creates a new Bot
+   *
+   * @param {string} label Label of the bot
+   * @param {{lat: number, lng: number}} destination Destination for the bot
+   */
+  constructor(label, destination) {
+    this.label = label;
+    this.destination = destination;
+    this.setRandomOrigin(this.destination);
+  }
+  /**
+   * Sets the origin to a random location around the given destination
+   * @param {{lat: number, lng: number}} destination An object containing latitude and longitude properties
+   */
+  setRandomOrigin(destination) {
+    this.position = {
+      lat:
+        destination.lat +
+        Bot.getRandomInt(0, 15) * 0.01 * (Math.random() < 0.5 ? -1 : 1),
+      lng:
+        destination.lng +
+        Bot.getRandomInt(0, 15) * 0.01 * (Math.random() < 0.5 ? -1 : 1)
+    };
+    this.origin = this.position;
+  }
+  /**Moves the bot a set distance
+   *
+   * @param {number} distance Distance to move the bot in meters
+   */
+  moveTowardsDestination(distance) {
+    if (this.getDistanceToDestination() !== 0) {
+      this.decreaseBattery(Bot.getRandomInt(10, 30));
+      if (this.battery >= 0 && this.hasCharge) {
+        if (this.getDistanceToDestination() > distance) {
+          let displacementPoint = computeDestinationPoint(
+            this.position,
+            distance,
+            getRhumbLineBearing(this.position, this.destination),
+            Bot.earthRadius
+          );
+          this.position = {
+            lat: displacementPoint.latitude,
+            lng: displacementPoint.longitude
           };
-          
-          const getDistance = function(p1, p2) {
-            var R = Bot.earthRadius; // Earth’s mean radius in meter
-            var dLat = rad(p2.lat - p1.lat);
-            var dLong = rad(p2.lng - p1.lng);
-            var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-              Math.cos(rad(p1.lat)) * Math.cos(rad(p2.lat)) *
-              Math.sin(dLong / 2) * Math.sin(dLong / 2);
-            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-            var d = R * c;
-            return d; // returns the distance in meter
-        }
-        return getDistance(this.position, this.destination) */
-      // return this.google.maps.geometry.spherical.computeDistanceBetween(this.position, this.destination)
-        return getDistance(this.position, this.destination)
-     }
-
-   static getRandomInt(min, max) {
-        return Math.floor(Math.random() * (max - min)) + min;
-    }
-    changeColor(RGBColorCode) {
-        this.color = RGBColorCode
-    }
-
-    decreaseBattery(amount) {
-        if (amount > this.battery) {
-            this.battery = 0
-            this.hasCharge = false
         } else {
-            this.battery -= amount
+          this.position = this.destination;
+          this.isAtDestination = true;
         }
-        
+      } else {
+        if (!this.isCharging) {
+          this.isCharging = true;
+          setTimeout(() => {
+            this.rechargeBattery();
+          }, 6000);
+        }
+      }
     }
-    rechargeBattery() {
-        this.battery = 100
-        this.hasCharge = true
-        this.isCharging = false
+  }
+  /**Gets the remaining distance between the bot's position and it's destination
+   *
+   * @returns Distance to arrive at set destination in meters
+   */
+  getDistanceToDestination() {
+    // return this.google.maps.geometry.spherical.computeDistanceBetween(this.position, this.destination)
+    return getDistance(this.position, this.destination);
+  }
+  /**
+   * Gets a random number
+   *
+   * @param {number} min Minimun possible integer
+   * @param {number} max Maximun possible integer
+   * @returns {number} Random Integer
+   */
+  static getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  /**Sets the color property for the bot
+   *
+   * @param {string} RGBColorCode RGB color (e.g. #FFFFFF)
+   */
+  changeColor(RGBColorCode) {
+    this.color = RGBColorCode;
+  }
+  /**
+   * Decrease the bot's battery by the amount specified
+   * @param {number} amount Amount to reduce the bot's battery
+   */
+  decreaseBattery(amount) {
+    if (amount > this.battery) {
+      this.battery = 0;
+      this.hasCharge = false;
+    } else {
+      this.battery -= amount;
     }
+  }
+  /**
+   * Set the bot to full charge
+   */
+  rechargeBattery() {
+    this.battery = 100;
+    this.hasCharge = true;
+    this.isCharging = false;
+  }
 }
 
-export default Bot
+export default Bot;
--- src/components/Map.vue
+++ src/components/Map.vue
@@ -2,304 +2,318 @@
   <div>
     <h1>Bot Race</h1>
     <div class="container">
-    <GmapMap ref="mapRef"
-      :center="center"
-      :zoom="11"
-      map-type-id="roadmap"
-      class="map"
-      
-      :options="mapOptions"
-      @click="setNewDestination"
+      <GmapMap
+        ref="mapRef"
+        :center="center"
+        :zoom="11"
+        map-type-id="roadmap"
+        class="map"
+        :options="mapOptions"
+        @click="setNewDestination"
       >
-
-      <GmapMarker
-        :key="index"
-        v-for="(m,index) in markers"
-        :position="m.position"
-        :clickable="false"
-        :draggable="false"
-        :title="m.label"
-        :animation="google && google.maps.Animation.DROP"
-        :label="{text: m.label, color: m.color, fontSize: '1.5em'}"
-        :icon="{
+        <GmapMarker
+          :key="index"
+          v-for="(m, index) in markers"
+          :position="m.position"
+          :clickable="false"
+          :draggable="false"
+          :title="m.label"
+          :animation="google && google.maps.Animation.DROP"
+          :label="{ text: m.label, color: m.color, fontSize: '1.5em' }"
+          :icon="{
             path: carIcon.icon[4],
             fillColor: m.color,
             fillOpacity: 1,
             strokeColor: '#000',
             strokeWeight: 2,
             scale: 0.05,
-            anchor: google && new google.maps.Point(carIcon.icon[0]/2,carIcon.icon[1]),
-            labelOrigin: google && new google.maps.Point(carIcon.icon[0]/2,carIcon.icon[1]+100)
-      }"
-     
-    />
-     <GmapMarker
-        :position="center"
-        :clickable="false"
-        :draggable="false"
-        :title="'Destino'"
-        :animation="google && google.maps.Animation.BOUNCE"
-        :icon="{
+            anchor:
+              google &&
+              new google.maps.Point(carIcon.icon[0] / 2, carIcon.icon[1]),
+            labelOrigin:
+              google &&
+              new google.maps.Point(carIcon.icon[0] / 2, carIcon.icon[1] + 100),
+          }"
+        />
+        <GmapMarker
+          :position="center"
+          :clickable="false"
+          :draggable="false"
+          :title="'Destino'"
+          :animation="google && google.maps.Animation.BOUNCE"
+          :icon="{
             path: google && google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
             fillColor: '#0ff',
             fillOpacity: 1,
             strokeColor: '#000',
             strokeWeight: 2,
             scale: 5,
-            
-      }"
-/>
+          }"
+        />
 
-<button class="mapbutton" id="addBot" @click="addNewBot">Agregar Nuevo Bot</button>
-<button class="mapbutton" id="changeDestination" @click="changeNewDestination">Cambiar destino</button>
-</GmapMap>
-<div class="infoBoard">
-  <h2>Destino lat: {{center.lat.toFixed(5)}}, lng: {{center.lng.toFixed(5)}} </h2>
- 
-<table class="botTable">
-  <thead>
-  <tr class="botRowHeader">
-    <th>Posición</th>
-    <th>Nombre</th>
-    <th>Distancia</th>
-    <th>Bateria</th>
-    <th>Status</th>
-    <th>Latitud</th>
-    <th>Longitud</th>
-  </tr>
-  </thead>
- <tbody>
-     <tr class="botRow" v-for="(marker,index) in markers" :key="index">
-    <td>{{index + 1}}</td>
-    <td>{{marker.label}}</td>
-    <td>{{marker.getDistanceToDestination()}}</td>
-    <td>{{marker.battery}}</td>
-    <td>{{marker.isCharging?'Cargando':'Activo'}}</td>
-    <td>{{marker.position.lat.toFixed(5)}}</td>
-    <td>{{marker.position.lng.toFixed(5)}}</td>
-  </tr>
- </tbody>
+        <button class="mapbutton" id="addBot" @click="addNewBot">
+          Agregar Nuevo Bot
+        </button>
+        <button
+          class="mapbutton"
+          id="changeDestination"
+          @click="changeNewDestination"
+        >
+          Cambiar destino
+        </button>
+      </GmapMap>
+      <div class="infoBoard">
+        <h2>
+          Destino lat: {{ center.lat.toFixed(5) }}, lng:
+          {{ center.lng.toFixed(5) }}
+        </h2>
 
-</table>
-</div>
-
-
+        <table class="botTable">
+          <thead>
+            <tr class="botRowHeader">
+              <th>Posición</th>
+              <th>Nombre</th>
+              <th>Distancia</th>
+              <th>Bateria</th>
+              <th>Status</th>
+              <th>Latitud</th>
+              <th>Longitud</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="botRow" v-for="(marker, index) in markers" :key="index">
+              <td>{{ index + 1 }}</td>
+              <td>{{ marker.label }}</td>
+              <td>{{ marker.getDistanceToDestination() }}</td>
+              <td>{{ marker.battery }}</td>
+              <td>{{ marker.isCharging ? "Cargando" : "Activo" }}</td>
+              <td>{{ marker.position.lat.toFixed(5) }}</td>
+              <td>{{ marker.position.lng.toFixed(5) }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
-
   </div>
 </template>
 
 <script>
-import Bot from './../bot'
-import { gmapApi } from 'gmap-vue'
-import { faCar } from '@fortawesome/free-solid-svg-icons'
-
-
+import Bot from "./../bot";
+import { gmapApi } from "gmap-vue";
+import { faCar } from "@fortawesome/free-solid-svg-icons";
 
 export default {
-  name: 'Map',
-  data(){
+  name: "Map",
+  data() {
     return {
       mapOptions: {
-  zoomControl: true,
-   mapTypeControl: false,
-   scaleControl: false,
-   streetViewControl: false,
-   rotateControl: false,
-   fullscreenControl: true,
-   disableDefaultUi: false,
-   styles: [
-      { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
-      { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
-      { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
-      {
-        featureType: "administrative.locality",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#d59563" }],
+        zoomControl: true,
+        mapTypeControl: false,
+        scaleControl: false,
+        streetViewControl: false,
+        rotateControl: false,
+        fullscreenControl: true,
+        disableDefaultUi: false,
+        styles: [
+          { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+          {
+            elementType: "labels.text.stroke",
+            stylers: [{ color: "#242f3e" }],
+          },
+          { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+          {
+            featureType: "administrative.locality",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#d59563" }],
+          },
+          {
+            featureType: "poi",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#d59563" }],
+          },
+          {
+            featureType: "poi.park",
+            elementType: "geometry",
+            stylers: [{ color: "#263c3f" }],
+          },
+          {
+            featureType: "poi.park",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#6b9a76" }],
+          },
+          {
+            featureType: "road",
+            elementType: "geometry",
+            stylers: [{ color: "#38414e" }],
+          },
+          {
+            featureType: "road",
+            elementType: "geometry.stroke",
+            stylers: [{ color: "#212a37" }],
+          },
+          {
+            featureType: "road",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#9ca5b3" }],
+          },
+          {
+            featureType: "road.highway",
+            elementType: "geometry",
+            stylers: [{ color: "#746855" }],
+          },
+          {
+            featureType: "road.highway",
+            elementType: "geometry.stroke",
+            stylers: [{ color: "#1f2835" }],
+          },
+          {
+            featureType: "road.highway",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#f3d19c" }],
+          },
+          {
+            featureType: "transit",
+            elementType: "geometry",
+            stylers: [{ color: "#2f3948" }],
+          },
+          {
+            featureType: "transit.station",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#d59563" }],
+          },
+          {
+            featureType: "water",
+            elementType: "geometry",
+            stylers: [{ color: "#17263c" }],
+          },
+          {
+            featureType: "water",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#515c6d" }],
+          },
+          {
+            featureType: "water",
+            elementType: "labels.text.stroke",
+            stylers: [{ color: "#17263c" }],
+          },
+        ],
       },
-      {
-        featureType: "poi",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#d59563" }],
-      },
-      {
-        featureType: "poi.park",
-        elementType: "geometry",
-        stylers: [{ color: "#263c3f" }],
-      },
-      {
-        featureType: "poi.park",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#6b9a76" }],
-      },
-      {
-        featureType: "road",
-        elementType: "geometry",
-        stylers: [{ color: "#38414e" }],
-      },
-      {
-        featureType: "road",
-        elementType: "geometry.stroke",
-        stylers: [{ color: "#212a37" }],
-      },
-      {
-        featureType: "road",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#9ca5b3" }],
-      },
-      {
-        featureType: "road.highway",
-        elementType: "geometry",
-        stylers: [{ color: "#746855" }],
-      },
-      {
-        featureType: "road.highway",
-        elementType: "geometry.stroke",
-        stylers: [{ color: "#1f2835" }],
-      },
-      {
-        featureType: "road.highway",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#f3d19c" }],
-      },
-      {
-        featureType: "transit",
-        elementType: "geometry",
-        stylers: [{ color: "#2f3948" }],
-      },
-      {
-        featureType: "transit.station",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#d59563" }],
-      },
-      {
-        featureType: "water",
-        elementType: "geometry",
-        stylers: [{ color: "#17263c" }],
-      },
-      {
-        featureType: "water",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#515c6d" }],
-      },
-      {
-        featureType: "water",
-        elementType: "labels.text.stroke",
-        stylers: [{ color: "#17263c" }],
-      },
-    ]
-  },
-      center: {lat: 0, lng: 0},
+      center: { lat: 0, lng: 0 },
       markers: [],
-      botNumber: Bot.getRandomInt(5,10),
+      botNumber: Bot.getRandomInt(5, 10),
       timer: null,
       minStep: 50,
       maxStep: 100,
       movementTick: 1000,
       allowNewDestination: false,
-      carIcon: faCar
-    }
-
+      carIcon: faCar,
+    };
   },
   computed: {
     google: gmapApi,
   },
-  
-  methods:{
-    changeNewDestination(){
-      this.allowNewDestination = true
-      window.alert('Haga click en el mapa para seleccionar el nuevo destino y reiniciar la carrera')
+
+  methods: {
+    changeNewDestination() {
+      this.allowNewDestination = true;
+      alert(
+        "Haga click en el mapa para seleccionar el nuevo destino y reiniciar la carrera"
+      );
     },
-    setNewDestination({latLng}){
-      if(this.allowNewDestination){
-        this.center = {lat: latLng.lat(), lng: latLng.lng()}
-        this.allowNewDestination=false
+    setNewDestination({ latLng }) {
+      if (this.allowNewDestination) {
+        this.center = { lat: latLng.lat(), lng: latLng.lng() };
+        this.allowNewDestination = false;
       }
-      
     },
 
-    addNewBot(){
-      this.markers.push(new Bot(`Bot #${this.markers.length}`, this.center))
-      if(this.timer === null){
-        this.start()
+    addNewBot() {
+      this.markers.push(new Bot(`Bot #${this.markers.length}`, this.center));
+      if (this.timer === null) {
+        this.start();
       }
     },
-    start(){
+    start() {
       this.timer = setInterval(() => {
-      this.markers.forEach((el) => {
-        el.moveTowardsDestination(Bot.getRandomInt(this.minStep,this.maxStep))
-        el.changeColor('#FFF000')
-      })
-      this.markers.sort((a,b) => {
-        return a.getDistanceToDestination() - b.getDistanceToDestination()
-      })
-      this.markers[0].changeColor('#00FF00')
-      this.markers[this.markers.length-1].changeColor('#FF0000')
-      if(this.markers.reduce((prev, next) => {
-        return next.getDistanceToDestination()
-      })===0){
-        clearInterval(this.timer)
-        this.timer = null
-      }
-      
-      }, this.movementTick)
-    }
+        this.markers.forEach((el) => {
+          el.moveTowardsDestination(
+            Bot.getRandomInt(this.minStep, this.maxStep)
+          );
+          el.changeColor("#FFF000");
+        });
+        this.markers.sort((a, b) => {
+          return a.getDistanceToDestination() - b.getDistanceToDestination();
+        });
+        this.markers[0].changeColor("#00FF00");
+        this.markers[this.markers.length - 1].changeColor("#FF0000");
+        if (
+          this.markers.reduce((prev, next) => {
+            return next.getDistanceToDestination();
+          }) === 0
+        ) {
+          clearInterval(this.timer);
+          this.timer = null;
+        }
+      }, this.movementTick);
+    },
   },
-  created(){
-  this.$getLocation({})
-  .then(coordinates => {
-    this.center = {lat: coordinates.lat,  lng: coordinates.lng}    
-  }).catch(() => {
-   alert('Esta aplicación requiere su ubicación') 
-  }
-  )
-
-  },
-  mounted(){
-    
-      this.$refs.mapRef.$mapPromise.then((map) => {
-        map.controls[this.google.maps.ControlPosition.TOP_LEFT].push(document.getElementById('addBot'))
-        map.controls[this.google.maps.ControlPosition.BOTTOM_CENTER].push(document.getElementById('changeDestination'))
+  created() {
+    this.$getLocation({})
+      .then((coordinates) => {
+        this.center = { lat: coordinates.lat, lng: coordinates.lng };
       })
+      .catch(() => {
+        alert("Esta aplicación requiere su ubicación");
+      });
+  },
+  mounted() {
+    this.$refs.mapRef.$mapPromise.then((map) => {
+      map.controls[this.google.maps.ControlPosition.TOP_LEFT].push(
+        document.getElementById("addBot")
+      );
+      map.controls[this.google.maps.ControlPosition.BOTTOM_CENTER].push(
+        document.getElementById("changeDestination")
+      );
+    });
   },
 
   watch: {
-    center: function(val){
-    
-      let arr = []
+    center: function (val) {
+      let arr = [];
       for (let index = 0; index < this.botNumber; index++) {
-         arr.push(new Bot(`Bot #${arr.length}`,val))
+        arr.push(new Bot(`Bot #${arr.length}`, val));
       }
-      this.markers = arr
-     if(this.timer) {clearInterval(this.timer)}
-        this.start()
-      },
-
+      this.markers = arr;
+      if (this.timer) {
+        clearInterval(this.timer);
+      }
+      this.start();
     },
-  }
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-.container{
+.container {
   display: flex;
   justify-content: space-evenly;
   align-items: flex-start;
   flex-direction: row;
   flex-wrap: wrap;
 }
-.botList{
+.botList {
   text-align: left;
 }
 
-.mapbutton{
+.mapbutton {
   margin: 1em;
   padding: 1em;
-  opacity: .7;
+  opacity: 0.7;
 }
-.botList{
+.botList {
   max-width: 25em;
-  .botItem{
+  .botItem {
     font-size: 2em;
   }
 }
@@ -314,8 +328,7 @@ export default {
   color: rgb(170, 38, 38);
 }
 
-.map{
-
+.map {
   width: 25em;
   height: 25em;
 
@@ -323,24 +336,23 @@ export default {
     width: 20em;
     height: 20em;
   }
-
 }
 
 table {
   border-collapse: collapse;
   width: 100%;
- 
-    display: block;
-    overflow-x: auto;
-    white-space: nowrap;
 
+  display: block;
+  overflow-x: auto;
+  white-space: nowrap;
 }
 
-td, th {
+td,
+th {
   border: 1px solid #dddddd;
-  padding: .1em;
+  padding: 0.1em;
 }
-.infoBoard{
+.infoBoard {
   overflow-x: auto;
 }
 </style>
